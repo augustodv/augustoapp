78a3af89af3bfb65800265c4b9d2c04454eeae28
Augustodv <augustodevera@gmail.com>
2021-10-14 20:49:24 -0300

    Connect Database Local  close #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { DispositivosModule } from './dispositivos/dispositivos.module';
 import { CaracteristicasModule } from './caracteristicas/caracteristicas.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Caracteristica } from './caracteristicas/entities/caracteristica.entity';
+import { Dispositivo } from './dispositivos/entities/dispositivo.entity';
+
 
 @Module({
-  imports: [DispositivosModule, CaracteristicasModule],
+  imports: [
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'admin',
+    database: 'augustoapp',
+    entities: [Dispositivo,Caracteristica],
+    synchronize: true,
+  }),DispositivosModule, CaracteristicasModule],
   controllers: [],
   providers: [],
 })
+
+
 export class AppModule {}
