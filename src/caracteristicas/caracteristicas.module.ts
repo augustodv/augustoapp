c9905300bae3eb3009f718ccc0c260a27ca566a0
Augustodv <augustodevera@gmail.com>
2021-10-18 21:34:30 -0300

    Update Module's close #8

--- src/caracteristicas/caracteristicas.module.ts
+++ src/caracteristicas/caracteristicas.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CaracteristicasController } from './caracteristicas.controller';
 import { CaracteristicasService } from './caracteristicas.service';
+import { Caracteristica } from './entities/caracteristica.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Caracteristica])],
     controllers: [CaracteristicasController],
     providers:[CaracteristicasService]
 
--- src/dispositivos/dispositivos.module.ts
+++ src/dispositivos/dispositivos.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DispositivosController } from './dispositivos.controller';
 import { DispositivosService } from './dispositivos.service';
+import { Dispositivo } from './entities/dispositivo.entity';
 
     @Module({
+      imports: [TypeOrmModule.forFeature([Dispositivo])],
         controllers: [DispositivosController],
         providers: [DispositivosService]
       })
 
 
 
-export class DispositivosModule {}
+export class DispositivosModule {
+  
+}
