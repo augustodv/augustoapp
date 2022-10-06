309d403f7adf50915a6bef9622bf2ac99f71ac6c
augustodv <39961339+augustodv@users.noreply.github.com>
2022-10-05 21:12:41 -0300

    Create test yarn (#12)

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![TEST](https://github.com/augustodv/augustoapp/actions/workflows/test.yml/badge.svg)](https://github.com/augustodv/augustoapp/actions/workflows/test.yml)
+
+
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo_text.svg" width="320" alt="Nest Logo" /></a>
 </p>
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -9,6 +9,7 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -17,8 +18,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/caracteristicas')
+      .expect(200);
+      
   });
 });
