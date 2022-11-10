89fde464cf13d6e0620721693d15ab257fe4b735
augustodv <39961339+augustodv@users.noreply.github.com>
2022-11-10 20:14:50 -0300

    test spec

--- src/caracteristicas/caracteristicas.service.spec.ts
+++ src/caracteristicas/caracteristicas.service.spec.ts
@@ -1,14 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CaracteristicasService } from './caracteristicas.service';
+import { Repository } from 'typeorm';
+import { Caracteristica } from './entities/caracteristica.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+export type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+};
+
+// @ts-ignore 
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+  findOne: jest.fn(entity => entity),
+  // ...
+}));
 
 describe('CaracteristicasService', () => {
+  
+  let repositoryMock: MockType<Repository<Caracteristica>>;
   let service: CaracteristicasService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CaracteristicasService],
+      providers: [CaracteristicasService, { provide: getRepositoryToken(Caracteristica), useFactory: repositoryMockFactory }],
     }).compile();
-
+    repositoryMock = module.get(getRepositoryToken(Caracteristica));
     service = module.get<CaracteristicasService>(CaracteristicasService);
   });
 
